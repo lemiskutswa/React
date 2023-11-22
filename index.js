068ef8433b68bcbf62dd6f6767ce08b05c7691b3
lemiskutswa <kutswalemis16@gmail.com>
2023-11-22 21:01:28 +0300

    installed react dependencies

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
-ReactDOM.render(<p>My name is Lemis</p>, document.getElementById("root"));//ReactDOM is a global variable made available by (ReactDom link in HTML)
+import React from "react"
 
-ReactDOM.render(<p>
-    <ul>Home</ul>
-    <ul>Blog</ul>
-    <ul>Contact</ul>
-</p>, document.getElementById("list"));
+import ReactDOM from "react-dom"
+
+// ReactDOM.render(<h1 className="header">My name is Lemis</h1>, document.getElementById("root"));//ReactDOM is a global variable made available by (ReactDom link in HTML)
+// //ReactDom library gives us access to the render method
+
+// const page = 
+// <ul>
+//     <li>Home</li>
+//     <li>Blog</li>
+//     <li>Contact</li>
+// </ul>; 
+
+// ReactDOM.render( 
+// page, document.getElementById("list")
+// );
+
+// console.log(page);
+
+//React Component
+//JSX is mostly about putting HTML elements in variables
+
+const navbar = (
+    <nav>
+        <h1>Lemis Website</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(navbar, document.getElementById("root"));

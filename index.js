7c3753be4d9ff7d8d5be640117ef03eb4719a93b
lemiskutswa <kutswalemis16@gmail.com>
2023-11-22 13:20:27 +0300

    rendering a list

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-ReactDOM.render(<h1>Hello, everyone</h1>, document.getElementById("root"));//ReactDOM is a global variable made available by (ReactDom link in HTML)
+ReactDOM.render(<p>My name is Lemis</p>, document.getElementById("root"));//ReactDOM is a global variable made available by (ReactDom link in HTML)
+
+ReactDOM.render(<p>
+    <ul>Home</ul>
+    <ul>Blog</ul>
+    <ul>Contact</ul>
+</p>, document.getElementById("list"));

84eb82c2821db8e96b1f25c2749040bb9d37abbf
lemiskutswa <kutswalemis16@gmail.com>
2023-11-25 11:10:29 +0300

    putting header component in a different file

---
+++ new-application/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <nav className="nav-bar">
+            <img src="./react-logo.png" className="nav-logo"/>
+            <ul className="nav-items">
+                <li>Pricing</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    );
+}
--- new-application/index.js
+++ new-application/index.js
@@ -1,38 +1,4 @@
-// ReactDOM.render(<h1 className="header">My name is Lemis</h1>, document.getElementById("root"));//ReactDOM is a global variable made available by (ReactDom link in HTML)
-// //ReactDom library gives us access to the render method
 
-//const { divide } = require("lodash");
-
-// const page = 
-// <ul>
-//     <li>Home</li>
-//     <li>Blog</li>
-//     <li>Contact</li>
-// </ul>; 
-
-// ReactDOM.render( 
-// page, document.getElementById("list")
-// );
-
-// console.log(page);
-
-//React Component
-//JSX is mostly about putting HTML elements in variables
-
-// const navbar = (
-//     <nav>
-//         <h1>Lemis Website</h1>
-//         <ul>
-//             <li>Pricing</li>
-//             <li>About</li>
-//             <li>Contact</li>
-//         </ul>
-//     </nav>
-// )
-
-//ReactDOM.render(navbar, document.getElementById("root"));
- 
-// ReactDOM.createRoot(document.getElementById("root")).render(navbar);
 
 function Header() {
     return (

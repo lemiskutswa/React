7b604669e98a80a8f037df766686a73de9ead130
lemiskutswa <kutswalemis16@gmail.com>
2023-11-26 15:52:02 +0300

    styling the nav bar

--- react_vite/react-info-site/src/App.jsx
+++ react_vite/react-info-site/src/App.jsx
@@ -5,7 +5,7 @@ import Header from "./components/Header"
 import MainContent from "./components/MainContent"
 import Footer from "./components/Footer"
 
-function App () {
+export default function App () {
   return (
   <div>
       <Header />
--- react_vite/react-info-site/src/components/Footer.jsx
+++ react_vite/react-info-site/src/components/Footer.jsx
@@ -1,9 +1,9 @@
 import React from "react"
 
-export default function Footer () {
+export default function Footer() {
     return (
       <div>
-      <footer className="footer">2023 Lemis Development. All rights reserved</footer>;
+      <footer className="footer">2023 Lemis Kutswa. All rights reserved</footer>;
   </div>
     )
   }

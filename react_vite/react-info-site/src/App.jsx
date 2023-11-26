2c8b909ddde33710be6fdac3e6ae306c65814e2e
lemiskutswa <kutswalemis16@gmail.com>
2023-11-26 12:09:54 +0300

    moved each component into its own file

--- react_vite/react-info-site/src/App.jsx
+++ react_vite/react-info-site/src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import './index.css'
-import Header from "./Header"
-import MainContent from "./MainContent"
-import Footer from "./Footer"
+import Header from "./components/Header"
+import MainContent from "./components/MainContent"
+import Footer from "./components/Footer"
 
 function App () {
   return (
--- react_vite/react-info-site/src/components/Header.jsx
+++ react_vite/react-info-site/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import logo from "./assets/react-logo.png"
+import logo from "./react-logo.png"
 
 export default function Header() {
     return (

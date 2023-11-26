1c2604b004dd0db753f2512273051d4896af80a0
lemiskutswa <kutswalemis16@gmail.com>
2023-11-26 21:13:11 +0300

    styling the main content section

--- react_vite/react-info-site/src/components/Header.jsx
+++ react_vite/react-info-site/src/components/Header.jsx
@@ -4,7 +4,10 @@ import logo from "./react-logo.png"
 export default function Header() {
     return (
         <nav className="nav-bar">
-            <img src={logo} alt="logo" className="nav-logo"/>
+            <div className="logo-title">
+                <img src={logo} alt="logo" className="nav-logo"/>
+                <h1>React Facts</h1>
+            </div>
             <ul className="nav-items">
                 <li>Pricing</li>
                 <li>About</li>
--- react_vite/react-info-site/src/components/MainContent.jsx
+++ react_vite/react-info-site/src/components/MainContent.jsx
@@ -2,15 +2,15 @@ import React from "react"
 
 export default function MainContent () {
     return (
-        <div>
-        <h1 className="h1">Fun Facts about React</h1>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100K stars on GitHub</li>
-            <li>Is maintained by Facebook</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
+        <div className="main-content">
+            <h1 className="h1">Fun Facts about React</h1>
+            <ul className="fun-facts-list">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
         </div>
   
     )

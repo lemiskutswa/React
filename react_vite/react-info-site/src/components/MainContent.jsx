fda0e3407325b31ad361e99fd194b29ecddb06e3
lemiskutswa <kutswalemis16@gmail.com>
2023-11-26 22:57:10 +0300

    added background logo

--- react_vite/react-info-site/src/components/MainContent.jsx
+++ react_vite/react-info-site/src/components/MainContent.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function MainContent () {
     return (
-        <div className="main-content">
+        <main className="main-content">
             <h1 className="h1">Fun Facts about React</h1>
             <ul className="fun-facts-list">
                 <li>Was first released in 2013</li>
@@ -11,7 +11,7 @@ export default function MainContent () {
                 <li>Is maintained by Facebook</li>
                 <li>Powers thousands of enterprise apps, including mobile apps</li>
             </ul>
-        </div>
+        </main>
   
     )
   }

fa2bcaf884fd2711e68d17d8cffe758c98e5052f
lemiskutswa <kutswalemis16@gmail.com>
2024-01-02 10:22:02 +0300

    replacing hard code with props

--- react_vite/air-bnb experiences/src/assets/components/Card.jsx
+++ react_vite/air-bnb experiences/src/assets/components/Card.jsx
@@ -1,23 +1,23 @@
 import React from "react"
-import CardImage from "../images/katie-zaferes.png"
+//import CardImage from "../images/katie-zaferes.png"
 import StarIcon from "../images/star.png"
 
-export default function Card() {
+export default function Card(props) {
     return(
         <div className="cards-section">
             <div className="card-element">
                 <div className="card-image-section">
-                    <img src= {CardImage} alt="Main card image" className="card-image"/>
+                    <img src= {`src/assets/images/${props.img}`} alt="Main card image" className="card-image"/>
                     <p className="sold-out-text">SOLD OUT</p>
                 </div>
                 <div className="card-content">
                     <div className="first-row">
                         <img src={StarIcon} alt="star-icon" className="star-icon"/>
-                        <p className="rating">5.0 <span className="location">(6) &middot; USA</span></p>
+                        <p className="rating">{props.rating} <span className="location">({props.reviewCount}) &middot; {props.country}</span></p>
                     </div>
                     <div className="labels">
-                        <h2>Life lessons with Katie Zaferes</h2>
-                        <p> <span className="price">From $136</span> / person</p>
+                        <h2>{props.title}</h2>
+                        <p> <span className="price">From ${props.price}</span> / person</p>
                     </div>
                 </div>
             </div>

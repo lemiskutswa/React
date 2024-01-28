784d02f35223dec0eda9f0d57a6c69e2b7782f2e
lemiskutswa <kutswalemis16@gmail.com>
2024-01-28 21:26:47 +0300

    Added new images to hero section

--- react_vite/air-bnb experiences/src/assets/components/Hero.jsx
+++ react_vite/air-bnb experiences/src/assets/components/Hero.jsx
@@ -1,10 +1,19 @@
 import React from "react"
-import photogrid from "../images/photo-grid.png"
+import MostPopularImage from "../images/most-popular.png"
+import TeamBuildingImage from "../images/team-building.png" 
+import FamilyFunImage from "../images/family-fun.png"
+import EasyPlanningImage from "../images/easy-planning.png"
 
 export default function Hero() {
     return (
         <section className="hero">
-            <img src={photogrid} className="hero--photo" />
+            <h1>New this week</h1>
+            <div className="new-this-week">
+                <img src={MostPopularImage} alt="most-popular-img" />
+                <img src={EasyPlanningImage} alt="easy-planning-img" />
+                <img src={FamilyFunImage} alt="family-fun-img" />
+                <img src={TeamBuildingImage} alt="team-building-img" />
+            </div>
             <h1 className="hero--header">Online Experiences</h1>
             <p className="hero--text">Join unique interactive activities led by 
             one-of-a-kind hosts—all without leaving home.</p>

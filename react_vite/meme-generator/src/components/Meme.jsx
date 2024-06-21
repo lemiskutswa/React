571f70f42e33403dfccbefb51e1d377b20fdf84a
lemiskutswa <lemiskutswa20@gmail.com>
2024-06-21 16:39:50 +0300

    add text to image

--- react_vite/meme-generator/src/components/Meme.jsx
+++ react_vite/meme-generator/src/components/Meme.jsx
@@ -24,8 +24,20 @@ export default function Meme() {
         });
     }
 
-      
-    
+    console.log(meme);
+
+    function handleChange(event) {
+        
+        const { value, name } = event.target;
+        setMeme(prevText => {
+            return {
+                ...prevText, 
+                [name]:value
+            }
+        })
+    }
+
+
     return(
             <main>
                 <div className="form">
@@ -33,11 +45,15 @@ export default function Meme() {
                         type="text"
                         placeholder="Top text"
                         className="form--input"
+                        name="topText"
+                        onChange={handleChange}
                     />
                     <input 
                         type="text"
                         placeholder="Bottom text"
                         className="form--input"
+                        name="bottomText"
+                        onChange={handleChange}
                     />
                     <button 
                         className="form--button"
@@ -46,7 +62,11 @@ export default function Meme() {
                         Get a new meme image 🖼
                     </button>
                 </div>
-                <img src={meme.randomImage} className="meme--image" />
+                <div className="meme">
+                    <img src={meme.randomImage} className="meme--image" />
+                    <h2 className="meme--text top">{meme.topText}</h2>
+                    <h2 className="meme--text bottom">{meme.bottomText}</h2>
+                </div>
             </main>
     )
 }
